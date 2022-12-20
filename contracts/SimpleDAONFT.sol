4b2231b054628b68d1c80b23a59295537dc41b18
Dharshan K <kdharshanprog@gmail.com>
2022-12-20 21:57:43 +0530

    bug fixed

--- contracts/SimpleDAONFT.sol
+++ contracts/SimpleDAONFT.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+/ SPDX-License-Identifier: MIT
 pragma solidity ^8.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
