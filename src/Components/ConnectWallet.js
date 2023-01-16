c9382620edf46c32cbeadbe002211c385be27f25
Dharshan K <kdharshanprog@gmail.com>
2023-01-16 19:19:59 +0530

    Connect button done

--- src/Components/ConnectWallet.js
+++ src/Components/ConnectWallet.js
@@ -6,7 +6,8 @@ import { useState, useEffect } from "react";
 
 export default function ConnectWallet() {
   const [correctNetwork, setCorrectNetwork] = useState(false);
-  const [currentAccount, setCurrentAccount] = useState();
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [connected, setConnected] = useState(false);
 
   const connectWallet = async () => {
     try {
@@ -22,50 +23,39 @@ export default function ConnectWallet() {
       if (chainId !== goerliId) {
         alert("not connected to goerli network");
         return;
+      } else {
+        setCorrectNetwork(true);
       }
 
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
+      setConnected(true);
 
       console.log("found accounts", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log("error connecting to metamask");
-    }
-  };
-  const checkCorrectNetwork = async () => {
-    const { ethereum } = window;
-    let chainId = await ethereum.request({ method: "eth_chainId" });
-    console.log(chainId);
-    // console.log("Connected to chain:" + chainId);
-
-    const rinkebyChainId = "0x5";
-
-    if (chainId !== rinkebyChainId) {
-      setCorrectNetwork(false);
-    } else {
-      setCorrectNetwork(true);
+      setConnected(false);
+      console.log(connected);
     }
   };
 
   // useEffect(() => {
-  //   connectWallet();
-  //   checkCorrectNetwork();
-  // }, []);
+  //   document.getElementById("connect").innerHTML = "connected";
+  //   setConnected(true);
+  // }, [connected]);
 
   return (
     <div>
       <div>
-        {correctNetwork === "" ? (
-          <button onClick={connectWallet}>Connect Wallet</button>
+        {connected === false ? (
+          <button id="connect" onClick={connectWallet}>
+            Connect Wallet
+          </button>
         ) : (
           <p>Connected</p>
         )}
-        <p>
-          {correctNetwork}
-          {currentAccount}
-        </p>
       </div>
       <h1>Hello world</h1>
       <button onClick={connectWallet}>Connect Wallet</button>
